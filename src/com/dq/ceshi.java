5ede382ef94ecc5e445b8de65989e2595edbb146
foreverLegend0413 <297887113@qq.com>
2018-07-10 15:29:25 +0800

    commit

--- src/com/dq/ceshi.java
+++ src/com/dq/ceshi.java
@@ -5,6 +5,8 @@ public class ceshi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ÍÛ¹ş¹ş£¡");
+		System.out.println("ÎûÎû");
+		System.out.println("gaga");
 	}
 
 }
